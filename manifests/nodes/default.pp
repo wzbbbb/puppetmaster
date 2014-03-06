aac52c4f061bec16d3c292c04b70d8e97e96ff8f
wang <zwa@orsyp.com>
2014-03-05 20:31:03 -0500

    modules 	modified:   ../../enc/raring64.orsypgroup.com.yaml

--- manifests/nodes/default.pp
+++ manifests/nodes/default.pp
@@ -1,23 +1,18 @@
 node default { #include everything
-  notify{"Installation with default node configuration.": }
-  Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ],}
-  exec { "apt-update": command => "apt-get update", }
-  include nginx,
-  include postfix,
-  include rvm,
-  include sidekiq,
-  include unicorn,
-  include redis-server,
-  include mongodb,
-  include git,
-  include mailutils,
-  exec { "capistrano script":
-      command => "ls",
-      require => Service['mongod'],
-      require => Service['redis-server'],
-      require => Service['nginx'],
-      require => Service['unicorn']
-      require => Service['sidekiq']
+  notify{'Installation with default node configuration.': }
+  Exec { path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],}
+  exec { 'apt-update': command => 'apt-get update', }
+  include nginx
+  include postfix
+  include rvm
+  include sidekiq
+  include unicorn
+  include redis-server
+  include mongodb
+  include git
+  include mailutils
+  exec { 'capistrano script':
+      command => 'ls',
+      require => Service['mongod', 'redis-server', 'nginx', 'unicorn', 'sidekiq'],
   }
-
 }
