b8ad2fe5cb1053ef2ac131e8ef6a992a700bade7
wang <zwa@orsyp.com>
2014-03-07 16:45:54 -0500

    modified:   default.pp 	modified:   ../../enc/default.yaml 	modified:   ../../enc/raring64.orsypgroup.com.yaml 	deleted:    sd_api.pp 	deleted:    sd_api.pp_bkp 	modified:   ../../modules/base/manifests/init.pp 	modified:   ../../modules/mongodb/manifests/init.pp 	deleted:    ../../modules/zhibing-git/Modulefile 	deleted:    ../../modules/zhibing-git/README 	deleted:    ../../modules/zhibing-git/manifests/init.pp 	deleted:    ../../modules/zhibing-git/spec/spec_helper.rb 	deleted:    ../../modules/zhibing-git/tests/init.pp 	deleted:    ../../modules/zhibing-mailutils/Modulefile 	deleted:    ../../modules/zhibing-mailutils/README 	deleted:    ../../modules/zhibing-mailutils/manifests/init.pp 	deleted:    ../../modules/zhibing-mailutils/spec/spec_helper.rb 	deleted:    ../../modules/zhibing-mailutils/tests/init.pp 	deleted:    ../../modules/zhibing-mongodb/Modulefile 	deleted:    ../../modules/zhibing-mongodb/README 	deleted:    ../../modules/zhibing-mongodb/manifests/init.pp 	deleted:    ../../modules/zhibing-mongodb/spec/spec_helper.rb 	deleted:    ../../modules/zhibing-mongodb/tests/init.pp 	deleted:    ../../modules/zhibing-nginx/Modulefile 	deleted:    ../../modules/zhibing-nginx/README 	deleted:    ../../modules/zhibing-nginx/files/nginx.conf 	deleted:    ../../modules/zhibing-nginx/manifests/init.pp 	deleted:    ../../modules/zhibing-nginx/spec/spec_helper.rb 	deleted:    ../../modules/zhibing-nginx/tests/init.pp 	deleted:    ../../modules/zhibing-postfix/Modulefile 	deleted:    ../../modules/zhibing-postfix/README 	deleted:    ../../modules/zhibing-postfix/manifests/init.pp 	deleted:    ../../modules/zhibing-postfix/spec/spec_helper.rb 	deleted:    ../../modules/zhibing-postfix/tests/init.pp 	deleted:    ../../modules/zhibing-redisserver/Modulefile 	deleted:    ../../modules/zhibing-redisserver/README 	deleted:    ../../modules/zhibing-redisserver/manifests/init.pp 	deleted:    ../../modules/zhibing-redisserver/spec/spec_helper.rb 	deleted:    ../../modules/zhibing-redisserver/tests/init.pp 	deleted:    ../../modules/zhibing-rvm/Modulefile 	deleted:    ../../modules/zhibing-rvm/README 	deleted:    ../../modules/zhibing-rvm/manifests/init.pp 	deleted:    ../../modules/zhibing-rvm/spec/spec_helper.rb 	deleted:    ../../modules/zhibing-rvm/tests/init.pp 	deleted:    ../../modules/zhibing-sidekiq/Modulefile 	deleted:    ../../modules/zhibing-sidekiq/README 	deleted:    ../../modules/zhibing-sidekiq/manifests/init.pp 	deleted:    ../../modules/zhibing-sidekiq/spec/spec_helper.rb 	deleted:    ../../modules/zhibing-sidekiq/tests/init.pp 	deleted:    ../../modules/zhibing-unicorn/Modulefile 	deleted:    ../../modules/zhibing-unicorn/README 	deleted:    ../../modules/zhibing-unicorn/manifests/init.pp 	deleted:    ../../modules/zhibing-unicorn/spec/spec_helper.rb 	deleted:    ../../modules/zhibing-unicorn/tests/init.pp

--- manifests/nodes/default.pp
+++ manifests/nodes/default.pp
@@ -1,10 +1,10 @@
 node default { #include everything
   notify{'Installation with default node configuration.': }
   Exec { path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],}
-  exec { 'apt-update': command => 'apt-get update', }
+  #exec { 'apt-update': command => 'apt-get update', }
   #include base
-  include nginx
-  include postfix
+  #include nginx
+  #include postfix
   #include rvm
   #rvm_system_ruby {
   #  'ruby-2.1.0':
@@ -14,16 +14,24 @@ node default { #include everything
   #}
   #include sidekiq
   #include unicorn
-  include redisserver
-  include mongodb
-  include git
-  include mailutils
-  exec { 'install ruby 2.1.0':
-    command => '/usr/local/rvm/bin/rvm install 2.1.0',
-    onlyif  => '/usr/local/bin/gem install rvm',
-  }
-  #exec { 'capistrano script':
-  #    command => 'ls',
-  #    require => Service['mongod', 'redis-server', 'nginx', 'unicorn', 'sidekiq'],
+  #include redisserver
+  #include mongodb
+  #include git
+  #include mailutils
+  #exec { 'install ruby 2.1.0':
+  #  command => 'su admin -c " /home/admin/.rvm/bin/rvm install 2.1.0"',
+  #  onlyif  => 'su admin -c "\curl -sSL https://get.rvm.io | bash -s stable"',
+  #  timeout => 1800,
+  #  require => User['admin']
   #}
+  exec { 'capistrano script':
+      command => 'su admin -c "cd ~/server; rvm 2.1.0 do cap local deploy"',
+      onlyif  =>[ 'su admin -c "cd ~; git clone -b develop git@192.168.114.174:smoke-detector/server.git"',
+                  'su admin -c "cd ~/server; rvm 2.1.0 do gem install bundler"',
+                  'su admin -c "cd ~/server; rvm 2.1.0 do bundle install"'],
+      timeout => 1800,
+      #require => Service['mongod', 'redis-server', 'nginx'],
+      require => Exec['install ruby 2.1.0'],
+
+  }
 }
