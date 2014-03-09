d4e0cfca646d24c71f635a9baf1e79e21ab4cc47
wang <zwa@orsyp.com>
2014-03-09 12:54:49 -0400

    modified:   default.pp

--- manifests/nodes/default.pp
+++ manifests/nodes/default.pp
@@ -1,8 +1,8 @@
 node default { #include everything
   notify{'Installation with default node configuration.': }
   Exec { path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],}
-  #exec { 'apt-update': command => 'apt-get update', }
-  #include base
+  exec { 'apt-update': command => 'apt-get update', }
+  include base
   #include nginx
   #include postfix
   #include rvm
@@ -18,25 +18,26 @@ node default { #include everything
   #include mongodb
   #include git
   #include mailutils
-  exec { 'install ruby 2.1.0':
-    command => 'su admin -c " /home/admin/.rvm/bin/rvm install 2.1.0"',
-    onlyif  => 'su admin -c "\curl -sSL https://get.rvm.io | bash -s stable"',
-    timeout => 1800,
-    require => User['admin'],
-  }
-  exec { 'clone repo':
-    command =>'su admin -c "cd ~; git clone -b develop git@192.168.114.174:smoke-detector/server.git"',
-    require => Exec['install ruby 2.1.0'],
-  }
-  exec { 'bundle install':
-    command=> 'su admin -c "cd ~/server; rvm 2.1.0 do bundle install"',
-    onlyif => 'su admin -c "cd ~/server; rvm 2.1.0 do gem install bundler"',
-    timeout=> 1800,
-    require=> Exec['clone repo'],
-  }
-  exec { 'capistrano script':
-    command => 'su admin -c "cd ~/server; rvm 2.1.0 do cap local deploy"',
-    timeout => 1800,
-    require => Exec['bundle install'],
-  }
+  include rubystack
+  #exec { 'install ruby 2.1.0':
+  #  command => 'su admin -c " /home/admin/.rvm/bin/rvm install 2.1.0"',
+  #  onlyif  => 'su admin -c "\curl -sSL https://get.rvm.io | bash -s stable"',
+  #  timeout => 1800,
+  #  require => User['admin'],
+  #}
+  #exec { 'clone repo':
+  #  command =>'su admin -c "cd ~; git clone -b develop git@192.168.114.174:smoke-detector/server.git"',
+  #  require => Exec['install ruby 2.1.0'],
+  #}
+  #exec { 'bundle install':
+  #  command=> 'su admin -c "cd ~/server; rvm 2.1.0 do bundle install"',
+  #  onlyif => 'su admin -c "cd ~/server; rvm 2.1.0 do gem install bundler"',
+  #  timeout=> 1800,
+  #  require=> Exec['clone repo'],
+  #}
+  #exec { 'capistrano script':
+  #  command => 'su admin -c "cd ~/server; rvm 2.1.0 do cap local deploy"',
+  #  timeout => 1800,
+  #  require => Exec['bundle install'],
+  #}
 }
